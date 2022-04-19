58517137160599eb80a811ebccf5c3dc197a48e1
IMS212 <ims@ims21.tech>
2022-04-19 05:11:24 -0700

    Fix animation

--- src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/pbr_animation/MixinTextureAtlasSprite.java
+++ src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/pbr_animation/MixinTextureAtlasSprite.java
@@ -1,7 +1,5 @@
 package net.coderbot.iris.compat.sodium.mixin.pbr_animation;
 
-import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,13 +9,12 @@ import me.jellysquid.mods.sodium.client.render.texture.SpriteUtil;
 import net.coderbot.iris.texture.pbr.PBRSpriteHolder;
 import net.coderbot.iris.texture.pbr.TextureAtlasSpriteExtension;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(TextureAtlas.class)
+@Mixin(TextureAtlasSprite.class)
 public class MixinTextureAtlasSprite {
-	@Inject(method = "getSprite", at = @At("RETURN"), remap = false)
-	private void onTailMarkActive(ResourceLocation arg, CallbackInfoReturnable<TextureAtlasSprite> cir) {
-		TextureAtlasSpriteExtension extension = (TextureAtlasSpriteExtension) cir.getReturnValue();
+	@Inject(method = "setActive(Z)V", at = @At("TAIL"), remap = false)
+	private void onTailMarkActive(CallbackInfo ci) {
+		TextureAtlasSpriteExtension extension = (TextureAtlasSpriteExtension) this;
 		if (extension.hasPBRHolder()) {
 			PBRSpriteHolder pbrHolder = extension.getPBRHolder();
 			TextureAtlasSprite normalSprite = pbrHolder.getNormalSprite();
